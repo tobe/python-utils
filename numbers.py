7c070f5c6d93ca58de09f20e860f181e0d3449c9
T.B <infy@cybershade.org>
2013-09-17 16:44:30 +0200

    core for numbers.py

---
+++ numbers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+def run():
+    workload = [] # Empty workload
+    data = ''; # This is user edited
+
+
+if __name__ == '__main__':
+    try:
+        run()
+    except KeyboardInterrupt:
+        exit('0')
