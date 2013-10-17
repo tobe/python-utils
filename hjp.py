73db6e2308aff6914293ba87fac34ef5e2d7fd9a
T.B <infy@cybershade.org>
2013-10-17 20:09:16 +0200

    Switched line endings to unix... :eyes:

---
+++ hjp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+import sys # We need this to use cmdline arguments
+import httplib # We need this in order to make the POST request.
+
+def main():
+        
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        quit('^C received, exiting...')
